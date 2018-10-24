ba59ecea562f9e1f9bebb335ce9f224e971b2a5f
BrunoMNDantas <bmndantas@gmail.com>
2018-10-24 18:58:19 +0100

    Resting client to work with new Server structure

--- ngspipes-share-client/src/app/logic/domain/group.ts
+++
@@ -1,9 +0,0 @@
-export class Group {
-    constructor(
-        public groupName : string,
-        public description : string,
-        public creationDate : Date,
-        public ownerName : string,
-        public members: string[]
-    ) { }
-}
