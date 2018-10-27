29abb8dfb8625efa69aa10e6194d4ed8d52a250b
BrunoMNDantas <bmndantas@gmail.com>
2018-10-27 19:53:44 +0100

    AppComponent subscribing theme event

--- ngspipes-share-client/src/app/app.component.ts
+++ ngspipes-share-client/src/app/app.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ThemeService } from '../services/theme.service';
+import { DialogManager } from '../components/dialog/dialog.manager';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+
+    themeChangeSubscription : any;
+
+    theme : string;
+
+
+
+    constructor(private themeService : ThemeService,
+                private dialogManager : DialogManager) { }
+
+
+
+    ngOnInit() {
+        this.themeChangeSubscription = this.themeService.themeChangeEvent.subscribe((theme) => {
+            this.loadTheme();
+        });
+
+        this.loadTheme();
+    }
+
+    ngOnDestroy() {
+        this.themeChangeSubscription.unsubscribe();
+    }
+
+    loadTheme() {
+        this.themeService.getTheme()
+            .then(theme => this.theme = theme)
+            .catch(error => this.dialogManager.openErrorDialog("Error getting theme!", error));
+    }
+
+}
