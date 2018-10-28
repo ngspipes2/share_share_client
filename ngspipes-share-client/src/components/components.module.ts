cb12bf8ff1b91ed498762a7ca7debca0f2c98b22
BrunoMNDantas <bmndantas@gmail.com>
2018-10-28 01:40:52 +0100

    RouterModule imported

--- ngspipes-share-client/src/components/components.module.ts
+++ ngspipes-share-client/src/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,19 +11,24 @@ import { SimpleDialogComponent } from './dialog/simple-dialog/simple-dialog.comp
 import { DialogManager } from './dialog/dialog.manager';
 import { UserImageComponent } from './image/user-image/user-image.component';
 
+const appRoutes: Routes = [ ];
+
 @NgModule({
     declarations: [
         SimpleDialogComponent,
         UserImageComponent
     ],
     imports: [
+        RouterModule,
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
         ReactiveFormsModule,
-        MaterialModule
+        MaterialModule,
+        RouterModule.forRoot(appRoutes /*,{enableTracing: true//debugging purposes only}*/)
     ],
     exports: [
+        RouterModule,
         SimpleDialogComponent,
         UserImageComponent
     ],
