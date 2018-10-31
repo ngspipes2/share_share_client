90e76cd8fd0747515dc080b379b43f7f1fdadaec
BrunoMNDantas <bmndantas@gmail.com>
2018-10-31 17:30:31 +0000

    Body module importing SideNav module

--- ngspipes-share-client/src/components/body/body.module.ts
+++ ngspipes-share-client/src/components/body/body.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../../app/material.module';
 import { FormsModule } from '@angular/forms';
+
+import { SideNavModule } from './side-nav/side-nav.module';
 
 import { BodyComponent } from './body.component';
 
@@ -13,10 +16,12 @@ import { BodyComponent } from './body.component';
     ],
     imports: [
         CommonModule,
+        RouterModule,
         BrowserModule,
         BrowserAnimationsModule,
         MaterialModule,
-        FormsModule
+        FormsModule,
+        SideNavModule
     ],
     exports: [
         BodyComponent
