bf6f5e72623d4e46936af9121a918d2fabe13231
BrunoMNDantas <bmndantas@gmail.com>
2018-10-31 17:30:53 +0000

    Body component using SideNav component

--- ngspipes-share-client/src/components/body/body.component.ts
+++ ngspipes-share-client/src/components/body/body.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { SessionService } from '../../services/session.service';
 
 @Component({
-  selector: 'app-body',
-  templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+    selector: 'app-body',
+    templateUrl: './body.component.html',
+    styleUrls: ['./body.component.scss']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    loginSubscription: any;
+    logoutSubscription: any;
+    loggedIn : boolean;
 
-  ngOnInit() {
-  }
+
+
+    constructor(private sessionService : SessionService) { }
+
+
+
+    ngOnInit() {
+        this.loginSubscription = this.sessionService.loginEvent.subscribe( () => {
+            this.loggedIn = true;
+        });
+
+        this.logoutSubscription = this.sessionService.logoutEvent.subscribe(() => {
+            this.loggedIn = false;
+        });
+
+        this.loggedIn = this.sessionService.isLoggedIn();
+    }
+
+    ngOnDestroy() {
+        this.loginSubscription.unsubscribe();
+        this.logoutSubscription.unsubscribe();
+    }
 
 }
