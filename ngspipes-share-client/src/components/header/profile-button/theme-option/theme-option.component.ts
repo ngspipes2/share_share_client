90112bc72a28b5fc5f0f6411a0bb30203f8c8e3e
BrunoMNDantas <bmndantas@gmail.com>
2018-11-02 16:38:07 +0000

    Preview themes

--- ngspipes-share-client/src/components/header/profile-button/theme-option/theme-option.component.ts
+++ ngspipes-share-client/src/components/header/profile-button/theme-option/theme-option.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { DialogManager } from '../../../dialog/dialog.manager';
 import { ThemeService } from '../../../../services/theme.service';
@@ -8,18 +8,41 @@ import { ThemeService } from '../../../../services/theme.service';
     templateUrl: './theme-option.component.html',
     styleUrls: ['./theme-option.component.scss']
 })
-export class ThemeOptionComponent {
+export class ThemeOptionComponent implements OnInit{
+
+    currentTheme : string;
+
+
 
     constructor(private dialogManager : DialogManager,
                 private themeService : ThemeService) { }
 
 
 
+    ngOnInit() {
+        this.themeService.getTheme()
+        .then(theme => this.currentTheme = theme)
+        .catch(error => {
+            this.dialogManager.openErrorDialog("Error getting theme!", error)
+            console.error(error);
+        });
+    }
+
+    showTheme(theme : string) {
+        this.themeService.setTheme(theme);
+    }
+
+    hideTheme(theme : string) {
+        this.setTheme(this.currentTheme);
+    }
+
     setTheme(theme : string) {
         this.themeService.setTheme(theme)
         .then((result) => {
             if(!result)
                 this.dialogManager.openErrorDialog("Theme could not be set!", null);
+            else
+                this.currentTheme = theme;
         })
         .catch((error) => {
             this.dialogManager.openErrorDialog("Error setting theme!", error);
