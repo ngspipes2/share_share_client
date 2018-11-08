cb1af8638f93807acd0e34599d372e90d538e519
BrunoMNDantas <bmndantas@gmail.com>
2018-11-08 11:43:40 +0000

    ExternalRepositoryImage and InternalRepositoryImage components added to Image module

--- ngspipes-share-client/src/components/image/image.module.ts
+++ ngspipes-share-client/src/components/image/image.module.ts
@@ -4,14 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../../app/material.module';
 
-
 import { UserImageComponent } from './user-image/user-image.component';
 import { GroupImageComponent } from './group-image/group-image.component';
+import { ExternalRepositoryImageComponent } from './external-repository-image/external-repository-image.component';
+import { InternalRepositoryImageComponent } from './internal-repository-image/internal-repository-image.component';
 
 @NgModule({
   declarations: [
       UserImageComponent,
-      GroupImageComponent
+      GroupImageComponent,
+      ExternalRepositoryImageComponent,
+      InternalRepositoryImageComponent
   ],
   imports: [
       BrowserModule,
@@ -20,7 +23,9 @@ import { GroupImageComponent } from './group-image/group-image.component';
   ],
   exports: [
       UserImageComponent,
-      GroupImageComponent
+      GroupImageComponent,
+      ExternalRepositoryImageComponent,
+      InternalRepositoryImageComponent
   ]
 })
 export class ImageModule { }
---
+++ ngspipes-share-client/src/components/image/internal-repository-image/internal-repository-image.component.ts
@@ -0,0 +1,146 @@
+import { Component, Input, OnInit, OnDestroy, ElementRef } from '@angular/core';
+
+import { InternalRepository, InternalRepositoryType } from '../../../entities/internal-repository';
+import { InternalRepositoryService } from '../../../services/internal-repository.service';
+import { RepositoryConfig } from '../../../entities/repository-config';
+import { RepositoryConfigService } from '../../../services/repository-config.service';
+import { ToolsRepositoryFacadeService } from '../../../services/tools-repository-facade.service';
+import { PipelinesRepositoryFacadeService } from '../../../services/pipelines-repository-facade.service';
+
+@Component({
+  selector: 'app-internal-repository-image',
+  templateUrl: './internal-repository-image.component.html',
+  styleUrls: ['./internal-repository-image.component.scss']
+})
+export class InternalRepositoryImageComponent implements OnInit, OnDestroy {
+
+    @Input()
+    repositoryName : string;
+    @Input()
+    spinnerDiameter : number = 100;
+    @Input()
+    spinnerColor : string = "accent";
+
+    repositoryUpdateSubscription : any;
+    configUpdateSubscription : any;
+
+    repository : InternalRepository;
+    repositoryConfig : RepositoryConfig;
+    imageData : any;
+
+    loading : boolean;
+    inited : boolean = false;
+    observer: IntersectionObserver;
+
+
+
+    constructor(private internalRepositoryService : InternalRepositoryService,
+                private repositoryConfigService : RepositoryConfigService,
+                private toolsRepositoryFacadeService : ToolsRepositoryFacadeService,
+                private pipelinesRepositoryFacadeService : PipelinesRepositoryFacadeService,
+                private element : ElementRef) {}
+
+
+
+    ngOnInit() {
+        this.observer = new IntersectionObserver(this.handleIntersect.bind(this));
+        this.observer.observe(this.element.nativeElement);
+
+        this.repositoryUpdateSubscription = this.internalRepositoryService.repositoryUpdateEvent.subscribe((repositoryName) => {
+            if(repositoryName === this.repositoryName && this.inited)
+                this.load();
+        });
+
+        this.configUpdateSubscription = this.repositoryConfigService.configUpdateEvent.subscribe((configName) => {
+            if(this.repositoryConfig && this.repositoryConfig.name === configName && this.inited)
+                this.load();
+        });
+    }
+
+    ngOnDestroy() {
+        this.observer.disconnect();
+        this.repositoryUpdateSubscription.unsubscribe();
+        this.configUpdateSubscription.unsubscribe();
+    }
+
+    handleIntersect(entries, observer) : void {
+        entries.forEach((entry: IntersectionObserverEntry) => {
+            if (entry.isIntersecting && !this.inited) {
+                this.inited = true;
+                this.load();
+            }
+        });
+    }
+
+    load() {
+        this.loadRepository()
+        .then(() => {
+            this.loadRepositoryConfig()
+            .then(() => {
+                if(this.repositoryConfig)
+                    this.loadImage();
+            })
+        });
+    }
+
+    loadRepository() : Promise<InternalRepository> {
+        this.loading = true;
+
+        return this.internalRepositoryService.getRepository(this.repositoryName)
+        .then(repository => {
+            this.loading = false;
+            this.repository = repository;
+            return repository;
+        })
+        .catch(error => {
+            this.loading = false;
+            this.imageData = undefined;
+            throw error;
+        });
+    }
+
+    loadRepositoryConfig() : Promise<RepositoryConfig> {
+        this.loading = true;
+
+        return this.repositoryConfigService.getConfigsForLocation(this.repository.location)
+        .then(configs => {
+            this.loading = false;
+            this.repositoryConfig = configs.length > 0 ? configs[0] : undefined;
+            return configs[0];
+        })
+        .catch(error => {
+            this.loading = false;
+            this.imageData = undefined;
+            throw error;
+        });
+    }
+
+    loadImage() : Promise<any> {
+        this.loading = true;
+
+        let promise : Promise<any>;
+        if(this.repository.type === InternalRepositoryType.PIPELINES)
+            promise = this.pipelinesRepositoryFacadeService.getRepositoryImage(this.repositoryConfig);
+        else
+            promise = this.toolsRepositoryFacadeService.getRepositoryImage(this.repositoryConfig)
+
+        return promise.then(image => {
+                this.loading = false;
+                this.imageData = image;
+                return image;
+            })
+            .catch(error => {
+                this.loading = false;
+                this.imageData = undefined;
+                throw error;
+            });
+    }
+
+    getIcon() : string {
+        if(!this.repository)
+            return "";
+
+        return this.repository.type === InternalRepositoryType.PIPELINES ? "insert_drive_file" : "build";
+    }
+
+}
