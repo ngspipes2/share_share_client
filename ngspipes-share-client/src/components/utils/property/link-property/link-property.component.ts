bef3dbfcb7752f53a7791171717681a045fec07c
BrunoMNDantas <bmndantas@gmail.com>
2018-11-27 02:55:08 +0000

    LinkProperty component added to Property module

---
+++ ngspipes-share-client/src/components/utils/property/link-property/link-property.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-link-property',
+  templateUrl: './link-property.component.html',
+  styleUrls: ['./link-property.component.scss']
+})
+export class LinkPropertyComponent {
+
+    @Input()
+    name : string;
+    @Input()
+    value : string;
+    @Input()
+    link : string;
+
+
+
+    constructor() { }
+
+}
--- ngspipes-share-client/src/components/utils/property/property.module.ts
+++ ngspipes-share-client/src/components/utils/property/property.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../../app/material.module';
 
@@ -8,27 +11,33 @@ import { StringPropertyComponent } from './string-property/string-property.compo
 import { NumberPropertyComponent } from './number-property/number-property.component';
 import { DatePropertyComponent } from './date-property/date-property.component';
 import { BooleanPropertyComponent } from './boolean-property/boolean-property.component';
+import { LinkPropertyComponent } from './link-property/link-property.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule
-  ],
-  declarations: [
-      PropertyComponent,
-      StringPropertyComponent,
-      NumberPropertyComponent,
-      DatePropertyComponent,
-      BooleanPropertyComponent
-  ],
-  exports: [
-      PropertyComponent,
-      StringPropertyComponent,
-      NumberPropertyComponent,
-      DatePropertyComponent,
-      BooleanPropertyComponent
-  ]
+    imports: [
+        RouterModule,
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule
+    ],
+    declarations: [
+        PropertyComponent,
+        StringPropertyComponent,
+        NumberPropertyComponent,
+        DatePropertyComponent,
+        BooleanPropertyComponent,
+        LinkPropertyComponent
+    ],
+    exports: [
+        PropertyComponent,
+        StringPropertyComponent,
+        NumberPropertyComponent,
+        DatePropertyComponent,
+        BooleanPropertyComponent,
+        LinkPropertyComponent
+    ]
 })
 export class PropertyModule { }
