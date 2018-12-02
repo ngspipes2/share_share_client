34835047446cfadcb440a746e8455340c55d50e4
BrunoMNDantas <bmndantas@gmail.com>
2018-12-02 16:47:00 +0000

    changeRepository image operations implemented

--- ngspipes-share-client/src/services/tools-repository-facade.service.ts
+++ ngspipes-share-client/src/services/tools-repository-facade.service.ts
@@ -28,25 +28,43 @@ export class ToolsRepositoryFacadeService {
         });
     }
 
-    public setRepositoryImage(repositoryConfig : RepositoryConfig, file : any) : Promise<boolean> {
-        let url = ServersRoutes.TOOLS_FACADE_SET_LOGO_ROUTE;
-        let data = {
-            repositoryLocation : repositoryConfig.location,
-            repositoryConfig : this.createConfig(repositoryConfig.configs)
-        };
-
-        return this.httpService.uploadFile(url, data, file)
-        .then(response => {
-            return response.status === 200;
-        });
-    }
-
     private createConfig(configs : Config[] ) : any {
         let config = {};
 
         configs.forEach(c => config[c.name] = c.value);
 
         return config;
+    }
+
+
+    public setRepositoryImage(repositoryConfig : RepositoryConfig, file : any) : Promise<boolean> {
+        return this.readFile(file)
+        .then(content => {
+            let url = ServersRoutes.TOOLS_FACADE_SET_LOGO_ROUTE;
+            let data = {
+                data : content,
+                repositoryLocation : repositoryConfig.location,
+                repositoryConfig : this.createConfig(repositoryConfig.configs)
+            };
+
+            return this.httpService.post(url, data)
+            .then(response => {
+                return response.status === 200;
+            });
+        });
+    }
+
+    private readFile(file : any) : Promise<any> {
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
+            fileReader.onload = e => {
+                let arrayBuffer = fileReader.result;
+                let array = new Uint8Array(arrayBuffer);
+                resolve(Array.from(array));
+            };
+            fileReader.onerror = e => reject(e);
+            fileReader.readAsArrayBuffer(file);
+        });
     }
 
 }
