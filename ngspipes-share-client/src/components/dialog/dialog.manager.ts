288d872e9bc122fa95bb909706abc5184386dcbf
BrunoMNDantas <bmndantas@gmail.com>
2018-12-08 15:53:33 +0000

    Dialog manager with methods to convert dialogs to Promises

--- ngspipes-share-client/src/components/dialog/dialog.manager.ts
+++ ngspipes-share-client/src/components/dialog/dialog.manager.ts
@@ -30,15 +30,32 @@ export class DialogManager {
 
 
 
-     public open<T, D = any, R = any>(componentOrTemplateRef: ComponentType<T> | TemplateRef<T>, config?: MatDialogConfig<D>): MatDialogRef<T, R> {
+    public toPromise<T>(dialog : MatDialogRef<any, DialogResult<T>>) : Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+             dialog.afterClosed().subscribe(result => {
+                 if(result.error)
+                     reject(result.error);
+                 else
+                     resolve(result.result);
+             });
+         });
+    }
+
+    public open<T, D = any, R = any>(componentOrTemplateRef: ComponentType<T> | TemplateRef<T>, config?: MatDialogConfig<D>): MatDialogRef<T, R> {
          return this.dialog.open(componentOrTemplateRef, config);
      }
+
 
     public openSimpleDialog(title : string, message : string, type : Type, responses : string[]) : MatDialogRef<SimpleDialogComponent, DialogResult<string>> {
         return this.open(SimpleDialogComponent, {
             data: {title : title, message : message, type : type, responses : responses}
         });
     }
+
+    public openSimpleDialogAsPromise(title : string, message : string, type : Type, responses : string[]) : Promise<string> {
+        return this.toPromise(this.openSimpleDialog(title, message, type, responses));
+    }
+
 
     public openErrorDialog(title : string, message : string, responses ? : string[]) : MatDialogRef<SimpleDialogComponent, DialogResult<string>> {
         if(!responses)
@@ -47,12 +64,22 @@ export class DialogManager {
         return this.openSimpleDialog(title, message, Type.ERROR, responses);
     }
 
+    public openErrorDialogAsPromise(title : string, message : string, responses ? : string[]) : Promise<string> {
+        return this.toPromise(this.openErrorDialog(title, message, responses));
+    }
+
+
     public openWarningDialog(title : string, message : string, responses ? : string[]) : MatDialogRef<SimpleDialogComponent, DialogResult<string>> {
         if(!responses)
             responses = ["Ok"];
 
         return this.openSimpleDialog(title, message, Type.WARNING, responses);
     }
+
+    public openWarningDialogAsPromise(title : string, message : string, responses ? : string[]) : Promise<string> {
+        return this.toPromise(this.openWarningDialog(title, message, responses));
+    }
+
 
     public openInformationDialog(title : string, message : string, responses ? : string[]) : MatDialogRef<SimpleDialogComponent, DialogResult<string>> {
         if(!responses)
@@ -61,6 +88,11 @@ export class DialogManager {
         return this.openSimpleDialog(title, message, Type.INFO, responses);
     }
 
+    public openInformationDialogAsPromise(title : string, message : string, responses ? : string[]) : Promise<string> {
+        return this.toPromise(this.openInformationDialog(title, message, responses));
+    }
+
+
     public openSuccessDialog(title : string, message : string, responses ? : string[]) : MatDialogRef<SimpleDialogComponent, DialogResult<string>> {
         if(!responses)
             responses = ["Ok"];
@@ -68,49 +100,109 @@ export class DialogManager {
         return this.openSimpleDialog(title, message, Type.SUCCESS, responses);
     }
 
+    public openSuccessDialogAsPromise(title : string, message : string, responses ? : string[]) : Promise<string> {
+        return this.toPromise(this.openSuccessDialog(title, message, responses));
+    }
+
+
     public openChangePasswordDialog() : MatDialogRef<ChangePasswordDialogComponent, DialogResult<any>> {
         return this.dialog.open(ChangePasswordDialogComponent);
     }
+
+    public openChangePasswordDialogAsPromise() : Promise<any> {
+        return this.toPromise(this.openChangePasswordDialog());
+    }
+
 
     public openNewGroupNameDialog() : MatDialogRef<NewGroupNameDialogComponent, DialogResult<string>> {
         return this.dialog.open(NewGroupNameDialogComponent);
     }
 
+    public openNewGroupNameDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openNewGroupNameDialog());
+    }
+
+
     public openSelectRepostiroyConfigDialog() : MatDialogRef<SelectRepositoryConfigDialogComponent, DialogResult<string>> {
         return this.dialog.open(SelectRepositoryConfigDialogComponent);
     }
+
+    public openSelectRepostiroyConfigDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openSelectRepostiroyConfigDialog());
+    }
+
 
     public openNewAccessTokenNameDialog() : MatDialogRef<NewAccessTokenNameDialogComponent, DialogResult<string>> {
         return this.dialog.open(NewAccessTokenNameDialogComponent);
     }
 
+    public openNewAccessTokenNameDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openNewAccessTokenNameDialog());
+    }
+
+
     public openShowTokenDialog(token : string) : MatDialogRef<ShowTokenDialogComponent, DialogResult<any>> {
         return this.dialog.open(ShowTokenDialogComponent, {data : token});
     }
+
+    public openShowTokenDialogAsPromise(token : string) : Promise<string> {
+        return this.toPromise(this.openShowTokenDialog(token));
+    }
+
 
     public openNewRepositoryNameDialog() : MatDialogRef<NewRepositoryNameDialogComponent, DialogResult<string>> {
         return this.dialog.open(NewRepositoryNameDialogComponent);
     }
 
+    public openNewRepositoryNameDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openNewRepositoryNameDialog());
+    }
+
+
     public openNewRepositoryLocationDialog() : MatDialogRef<NewRepositoryLocationDialogComponent, DialogResult<string>> {
         return this.dialog.open(NewRepositoryLocationDialogComponent);
     }
+
+    public openNewRepositoryLocationDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openNewRepositoryLocationDialog());
+    }
+
 
     public openSelectRepositoryEntityTypeDialog() : MatDialogRef<SelectRepositoryEntityTypeDialogComponent, DialogResult<EntityType>> {
         return this.dialog.open(SelectRepositoryEntityTypeDialogComponent);
     }
 
+    public openSelectRepositoryEntityTypeDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openSelectRepositoryEntityTypeDialog());
+    }
+
+
     public openSelectUserDialog() : MatDialogRef<SelectUserDialogComponent, DialogResult<string>> {
         return this.dialog.open(SelectUserDialogComponent);
     }
+
+    public openSelectUserDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openSelectUserDialog());
+    }
+
 
     public openSelectGroupDialog() : MatDialogRef<SelectGroupDialogComponent, DialogResult<string>> {
         return this.dialog.open(SelectGroupDialogComponent);
     }
 
+    public openSelectGroupDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openSelectGroupDialog());
+    }
+
+
     public openSelectRepositoryDialog() : MatDialogRef<SelectRepositoryDialogComponent, DialogResult<string>> {
         return this.dialog.open(SelectRepositoryDialogComponent);
     }
+
+    public openSelectRepositoryDialogAsPromise() : Promise<string> {
+        return this.toPromise(this.openSelectRepositoryDialog());
+    }
+
 
     public openNewToolNameDialog(repositoryName : string) : MatDialogRef<NewToolNameDialogComponent, DialogResult<string>> {
         return this.dialog.open(NewToolNameDialogComponent, {
@@ -118,10 +210,19 @@ export class DialogManager {
         });
     }
 
+    public openNewToolNameDialogAsPromise(repositoryName : string) : Promise<string> {
+        return this.toPromise(this.openNewToolNameDialog(repositoryName));
+    }
+
+
     public openNewPipelineNameDialog(repositoryName : string) : MatDialogRef<NewPipelineNameDialogComponent, DialogResult<string>> {
         return this.dialog.open(NewPipelineNameDialogComponent, {
             data: { repositoryName : repositoryName }
         });
+    }
+
+    public openNewPipelineNameDialogAsPromise(repositoryName : string) : Promise<string> {
+        return this.toPromise(this.openNewPipelineNameDialog(repositoryName));
     }
 
 }
